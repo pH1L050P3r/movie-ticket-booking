c4edb5ae5e9a2ab3d8bcf40d9c086ca51b8f6fac
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-02-04 23:42:26 +0530

    Add delete booking apis

--- booking/src/main/java/com/booking/booking/service/BookingService.java
+++ booking/src/main/java/com/booking/booking/service/BookingService.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.booking.booking.dto.BookingCreateRequestDTO;
 import com.booking.booking.dto.BookingResponseDTO;
+import com.booking.booking.enums.Action;
+import com.booking.booking.exceptions.BookingServiceException;
+import com.booking.booking.exceptions.WallerServiceException;
 import com.booking.booking.mapper.BookingMapper;
 import com.booking.booking.models.Booking;
 import com.booking.booking.models.Show;
@@ -23,18 +27,23 @@ public class BookingService implements IBookingService {
     private BookingRepositories bookingRepositories;
     @Autowired
     private ShowRepositories showRepositories;
+    @Autowired
+    private IWalletClientService walletClientService;
 
-    public BookingResponseDTO createBooking(BookingCreateRequestDTO bookingRequestData){
+    public BookingResponseDTO createBooking(BookingCreateRequestDTO bookingRequestData) throws WallerServiceException, BookingServiceException{
         Booking booking = new Booking();
-        Show show = showRepositories.findById(bookingRequestData.getShowId()).get();
+        Show show;
         Long price = 0L;
 
         //Todo : Check user exists or not
-        //Todo : Check show exisxt or not
-
-        if(show.getSeatsAvailable() < bookingRequestData.getSeatsBooked()){
-            //Todo : Throw Exception becaouse seats are not available
+        try{
+            show = showRepositories.findById(bookingRequestData.getShowId()).get();
+        } catch(NoSuchElementException e){
+            throw new BookingServiceException("Show with specified id does not exists");
         }
+
+        if(show.getSeatsAvailable() < bookingRequestData.getSeatsBooked())
+            throw new BookingServiceException("Required no of seats are not available");
 
         booking.setSeatsBooked(bookingRequestData.getSeatsBooked());
         booking.setUserId(bookingRequestData.getUserId());
@@ -43,7 +52,12 @@ public class BookingService implements IBookingService {
         
         price += show.getPrice() * booking.getSeatsBooked();
         
-        //Todo : debit money from Wallet with price
+        //debit money from Wallet with price
+        try{
+            walletClientService.updateUserWalletMoney(price, bookingRequestData.getUserId(), Action.debit);
+        } catch(Exception e){
+            throw new WallerServiceException(e.getMessage());
+        }
 
         bookingRepositories.save(booking);
         return BookingMapper.mapBookingToBookingResponseDTO(booking);
@@ -56,39 +70,43 @@ public class BookingService implements IBookingService {
         return bookings;
     }
 
-    public void deleteUserBookings(Long userId){
+    public void deleteUserBookings(Long userId) throws BookingServiceException{
         List<Booking> bookings = bookingRepositories.findAllByUserId(userId);
         this.deleteBookingHelper(bookings);
     }
 
-    public void deleteUserShowBookings(Long userId, Long showId){
+    public void deleteUserShowBookings(Long userId, Long showId) throws BookingServiceException{
         List<Booking> bookings = bookingRepositories.findAllByUserIdAndShowId(userId, showId);
         this.deleteBookingHelper(bookings);
     }
 
-    public void deleteAllBookings(){
+    public void deleteAllBookings() throws BookingServiceException{
         List<Booking> booking = bookingRepositories.findAll();
         this.deleteBookingHelper(booking);
     }
 
-    private void deleteBookingHelper(List<Booking> bookings){
+    private void deleteBookingHelper(List<Booking> bookings) throws BookingServiceException{
         if(bookings.isEmpty()){
-            //TODO : Raise Exception and catch Controller
+            throw new BookingServiceException("Bookings does not exists");
         }
+
         Map<Long, Show> Shows = new HashMap<>();
-        Long totalPrice = 0L;
+        Map<Long, Long> userAmountRefund = new HashMap<>();
 
         for(Booking booking : bookings){
             Shows.put(booking.getShow().getId(), booking.getShow());
+            userAmountRefund.put(booking.getUserId(), 0L);
         }
 
         for(Booking booking : bookings){
             Show show = Shows.get(booking.getShow().getId());
             show.setSeatsAvailable(show.getSeatsAvailable() + booking.getSeatsBooked());
-            totalPrice += show.getPrice();
+            userAmountRefund.put(booking.getUserId(), userAmountRefund.get(booking.getUserId()) + show.getPrice()*booking.getSeatsBooked());
         }
-
-        //TODO : API request to Add money in Wallet
+        //API request to Add money in Wallet
+        for(Long userId : userAmountRefund.keySet()){
+            walletClientService.updateUserWalletMoney(userAmountRefund.get(userId), userId, Action.credit);
+        }
         bookingRepositories.deleteAll(bookings);
     }
 }
--- booking/src/main/java/com/booking/booking/service/IBookingService.java
+++ booking/src/main/java/com/booking/booking/service/IBookingService.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import com.booking.booking.dto.BookingCreateRequestDTO;
 import com.booking.booking.dto.BookingResponseDTO;
+import com.booking.booking.exceptions.BookingServiceException;
+import com.booking.booking.exceptions.WallerServiceException;
 
 public interface IBookingService {
-    public BookingResponseDTO createBooking(BookingCreateRequestDTO bookingRequestData);
+    public BookingResponseDTO createBooking(BookingCreateRequestDTO bookingRequestData)throws WallerServiceException,BookingServiceException;
     public List<BookingResponseDTO> getUserBookings(Long userId);
-    public void deleteUserBookings(Long userId);
-    public void deleteUserShowBookings(Long userId, Long showId);
-    public void deleteAllBookings();
+    public void deleteUserBookings(Long userId) throws BookingServiceException;
+    public void deleteUserShowBookings(Long userId, Long showId) throws BookingServiceException;
+    public void deleteAllBookings() throws BookingServiceException;
 }
