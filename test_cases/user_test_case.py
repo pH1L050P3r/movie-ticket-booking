6eea3ca0a77995cd6e33d8fd9b743f886203bccd
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-02-13 02:36:45 +0530

    Fixed user test cases

--- test_cases/user_test_case.py
+++ test_cases/user_test_case.py
@@ -9,21 +9,29 @@ walletServiceURL = "http://localhost:8082"
 # ANSI escape codes for colors
 RED = '\033[91m'
 RESET = '\033[0m'
+GREEN = '\u001b[32m'
 
 
 
 name = "Alice"
 email = "alice@example.com"
 
+USER_SECOND = {
+    "name" : "Bod",
+    "email" : "bob@example.com"
+}
+
+
 ############################################# USER END POINTS ###########################################
 #Create User
 def create_user(name, email):
+    delete_all_users()
     new_user = {"name": name, "email": email}
     response = requests.post(userServiceURL + "/users", json=new_user)
     return response
 
 #Delete User
-def delete_users():
+def delete_all_users():
     requests.delete(userServiceURL+f"/users") 
 
 #Get User
@@ -35,6 +43,7 @@ def get_user(user_id):
 
 # Test case for creating a new user with valid data
 def test_create_user():
+    delete_all_users()
     response_valid = create_user(name, email)
     if(response_valid.status_code != 201):
         print(RED + "Failed to create user with valid data" + RESET)
@@ -46,7 +55,7 @@ def test_create_user():
 # Test case for creating ag user with an existing email
 def test_existing_email():
     response_existing_email = create_user(name, email)
-    if(response_existing_email.status_code != 400):
+    if(response_existing_email.status_code == 400):
         print(RED + "Creating user with existing email should return 400" + RESET)
     else:
         print("User creation with existing email response:", response_existing_email.status_code)
@@ -54,9 +63,10 @@ def test_existing_email():
 
 # Test case for creating a user with missing name
 def test_missing_name():
+    delete_all_users()
     email_missing_name = "bob@example.com"
     response_missing_name = create_user("", email_missing_name)
-    if(response_missing_name.status_code != 400):
+    if(response_missing_name.status_code == 400):
         print(RED + "Creating user with missing name should return 400" + RESET)
     else:
         print("User creation with missing name response:", response_missing_name.status_code)
@@ -64,6 +74,7 @@ def test_missing_name():
 
 # Test case for creating a user with missing email
 def test_missing_email():
+    delete_all_users()
     name_missing_email = "Bob"
     response_missing_email = create_user(name_missing_email, "")
     if(response_missing_email.status_code != 400):
@@ -72,6 +83,7 @@ def test_missing_email():
         print("User creation with missing email response:", response_missing_email.status_code)
 
 def check_email_format():
+    delete_all_users()
     # Test case for valid Email format
     name = "Bob"
     email = "invalid_email"
@@ -82,6 +94,7 @@ def check_email_format():
         print("User creation with invalid email format response:", response.status_code)
     
 def test_missing_name_and_email():
+    delete_all_users()
     # Test case for empty name and email
     name = ""
     email = ""
@@ -92,8 +105,12 @@ def test_missing_name_and_email():
          print("User creation with empty name and email response:", response.status_code)
 
 def test_get_user_existing():
-    user_id = 1  # Assuming user with ID 1 exists 
-    response = get_user(user_id)
+    delete_all_users()
+    user = create_user(USER_SECOND.get("name"), USER_SECOND.get("email"))
+    if(user.status_code != 201):
+        print(RED + "Failed to get existing user" + RESET)
+
+    response = get_user(user.json().get("id"))
     if(response.status_code != 200):
         print(RED + "Failed to get existing user" + RESET)
     else:
