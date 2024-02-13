06a964efb9683f24a2c49dcab5b101a5628114f1
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-02-13 14:53:47 +0530

    Added test runner class in user test case

--- test_cases/user_test_case.py
+++ test_cases/user_test_case.py
@@ -1,5 +1,4 @@
 import requests
-import sys
 
 userServiceURL = "http://localhost:8080"
 bookingServiceURL = "http://localhost:8081"
@@ -12,18 +11,43 @@ RESET = '\033[0m'
 GREEN = '\u001b[32m'
 
 USER_FIRST = {
-    "name" : "Alice",
-    "email" : "alice@example.com"
+    "name" : "user1",
+    "email" : "user1@example.com"
 }
 
 USER_SECOND = {
-    "name" : "Bod",
-    "email" : "bob@example.com"
+    "name" : "user2",
+    "email" : "user2@example.com"
 }
 
 fail_print = lambda message : print(f"{message} : " + RED + "FAIL" + RESET)
 pass_print = lambda message : print(f"{message} : " + GREEN + "PASS" + RESET)
 
+
+class TestCaseRunner():
+    def __init__(self):
+        self.test_cases = []
+        self.userServiceURL = "http://localhost:8080"
+        self.bookingServiceURL = "http://localhost:8081"
+        self.walletServiceURL = "http://localhost:8082"
+    
+    def run(self):
+        self.setUp()
+        for func in self.test_cases:
+            self.fixture(func)
+
+    def test(self, func):
+        self.test_cases.append(func)
+    
+    def setUp(self):
+        pass
+    
+    def fixture(self, func):
+        # delete all user before every test case
+        requests.delete(userServiceURL+f"/users") 
+        func()
+
+test_runner = TestCaseRunner()
 
 ############################################# USER END POINTS ###########################################
 #Create User
@@ -53,18 +77,31 @@ def get_wallet(walletId):
 ################################################# TESTING ##############################################
 
 # Test case for creating a new user with valid data
+@test_runner.test
 def test_create_user():
-    delete_all_users()
     response_valid = create_user(USER_FIRST.get("name"), USER_FIRST.get("email"))
     if(response_valid.status_code != 201):
         fail_print("Test Create User")
     else:
         pass_print("Test Create User")
 
+@test_runner.test
+def test_create_user_response_format():
+    response_valid = create_user(USER_FIRST.get("name"), USER_FIRST.get("email"))
+    if(response_valid.status_code != 201):
+        fail_print("Test Create User response format")
+        return
+    for key in ["id", "name", "email"]:
+        if key not in response_valid.json().keys():
+            fail_print("Test Create User response format")
+            return
+    
+    pass_print("Test Create User response format")
+
 
 # Test case for creating ag user with an existing email
+@test_runner.test
 def test_existing_email():
-    delete_all_users()
     response_user = create_user(USER_FIRST.get("name"), USER_FIRST.get("email"))
     response_existing_email = create_user(USER_FIRST.get("name"), USER_FIRST.get("email"))
 
@@ -75,8 +112,8 @@ def test_existing_email():
 
 
 # Test case for creating a user with missing name
+@test_runner.test
 def test_missing_name():
-    delete_all_users()
     email_missing_name = "bob@example.com"
     response_missing_name = create_user("", email_missing_name)
     if(response_missing_name.status_code == 400):
@@ -86,8 +123,8 @@ def test_missing_name():
 
 
 # Test case for creating a user with missing email
+@test_runner.test
 def test_missing_email():
-    delete_all_users()
     name_missing_email = "Bob"
     response_missing_email = create_user(name_missing_email, "")
     if(response_missing_email.status_code != 400):
@@ -95,8 +132,9 @@ def test_missing_email():
     else:
         pass_print("Test Create User with missing email")
 
+
+@test_runner.test
 def test_check_email_format():
-    delete_all_users()
     # Test case for valid Email format
     name = "Bob"
     email = "invalid_email"
@@ -105,20 +143,10 @@ def test_check_email_format():
         fail_print("Test Create User with invalid email")
     else:
         pass_print("Test Create User with invalid email")
-    
-def test_missing_name_and_email():
-    # Test case for empty name and email
-    delete_all_users()
-    name = ""
-    email = ""
-    response = create_user(name, email)
-    if(response.status_code != 400):
-        print("Test Create User with missing email and name : " + RED + "FAIL" + RESET)
-    else:
-        print("Test Create User with invalid email : " + GREEN + "PASS" + RESET)
 
+
+@test_runner.test
 def test_get_existing_user():
-    delete_all_users()
     user = create_user(USER_SECOND.get("name"), USER_SECOND.get("email"))
 
     response = get_user(user.json().get("id"))
@@ -127,8 +155,24 @@ def test_get_existing_user():
     else:
         pass_print("Test Get existing user")
     
+
+@test_runner.test
+def test_get_user_response_format():
+    response_valid = create_user(USER_FIRST.get("name"), USER_FIRST.get("email"))
+    response_valid = get_user(response_valid.json().get("id"))
+    if(response_valid.status_code != 200):
+        fail_print("Test Create User response format")
+        return
+    for key in ["id", "name", "email"]:
+        if key not in response_valid.json().keys():
+            fail_print("Test Get User response format")
+            return
+    
+    pass_print("Test Get User response format")
+
+
+@test_runner.test
 def test_delete_user():
-    delete_all_users()
     response_user = create_user(USER_SECOND.get("name"), USER_SECOND.get("email"))
     delete_user(response_user.json().get("id"))
     response_existing_user = get_user(response_user.json().get("id"))
@@ -139,8 +183,8 @@ def test_delete_user():
         pass_print("Test delete user")
 
 
+@test_runner.test
 def test_delete_all_users():
-    delete_all_users()
     user_first = create_user(USER_FIRST.get("name"), USER_FIRST.get("email"))
     user_second = create_user(USER_SECOND.get("name"), USER_SECOND.get("email"))
     delete_all_users()
@@ -154,21 +198,10 @@ def test_delete_all_users():
 
 
 ########################################################################################################
-    
-def main():
-    try:
-        test_create_user()
-        test_existing_email()
-        test_missing_name()
-        test_missing_email()
-        test_check_email_format()
-        test_missing_name_and_email()
-        test_get_existing_user()
-        test_delete_user()
-        test_delete_all_users()
-    except Exception as e:
-        print("Something went wrong")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        test_runner.run()
+    except Exception as e:
+        print("Something went wrong")
