edbed1dc723565b592ebdc81b9d5988728d222a4
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-02-13 15:11:30 +0530

    Fix theatre get api throwing exception due to object to json mapping conflict

--- booking/src/main/java/com/booking/booking/controller/TheatreController.java
+++ booking/src/main/java/com/booking/booking/controller/TheatreController.java
@@ -1,6 +1,7 @@
 package com.booking.booking.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.booking.booking.models.Theatre;
+import com.booking.booking.dto.TheatreDTO;
+import com.booking.booking.mapper.TheatreMapper;
 import com.booking.booking.repositories.TheatreRepositories;
 
 @RestController
@@ -17,7 +19,10 @@ public class TheatreController {
     private TheatreRepositories threadRepositories;
 
     @GetMapping("/theatres")
-    public ResponseEntity<List<Theatre>> getAllTheatres(){
-        return new ResponseEntity<>(threadRepositories.findAll(), HttpStatus.OK);
+    public ResponseEntity<List<TheatreDTO>> getAllTheatres(){
+        return new ResponseEntity<>(
+            threadRepositories.findAll().stream().map(
+                show -> {return TheatreMapper.mapTheatreToTheatreDTO(show);}
+            ).collect(Collectors.toList()), HttpStatus.OK);
     }
 }
