9d812f184d3c73d42d65d431186c960c10dc4e81
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-03-03 00:53:00 +0530

    updated launch.sh

--- Phase 2/launch.sh
+++ Phase 2/launch.sh
@@ -26,12 +26,10 @@ kubectl apply -f wallet/service.yaml
 kubectl apply -f booking/service.yaml
 kubectl apply -f h2-db-service/service.yaml
 
+echo "Wait for 30 seconds"
+sleep 30
 kubectl port-forward service/user-service 8080:8080 &
 kubectl port-forward service/booking-service 8081:8081 &
 kubectl port-forward service/wallet-service 8082:8082 &
 
-
-# Expose main three services at ports 8080, 8081, and 8082
-# minikube service <team-member1-service-name> --url | sed 's/.*:\([0-9]*\)$/\1/' | xargs -I {} kubectl port-forward service/<team-member1-service-name> 8080:{}
-# minikube service <team-member2-service-name> --url | sed 's/.*:\([0-9]*\)$/\1/' | xargs -I {} kubectl port-forward service/<team-member2-service-name> 8081:{}
-# minikube service <team-member3-service-name> --url | sed 's/.*:\([0-9]*\)$/\1/' | xargs -I {} kubectl port-forward service/<team-member3-service-name> 8082:{}
+echo "Cluster Ready to run..."
