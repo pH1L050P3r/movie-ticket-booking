0dd75d3f2fc785fec43e62a984a9db40fd14c6ae
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-03-03 15:28:35 +0530

    Added Test case

--- Phase 2/teardown.sh
+++ Phase 2/teardown.sh
@@ -6,7 +6,8 @@ kubectl delete service user-service booking-service wallet-service h2db-service
 # Deleting Deployments
 kubectl delete deployment user-service booking-service wallet-service h2db-service
 
-sleep 30
+sleep 60
 
 # Deleting images
+eval $(minikube docker-env)
 docker rmi user-service:v0 booking-service:v0 wallet-service:v0 h2db-service:v0
